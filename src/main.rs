2eafa649d0f34782fc826b2a1cf8bb493f4f922d
grantt <justtonowu@gmail.com>
2024-12-19 02:00:07 +0100

    update current date in age calculator function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
     fn age_calculator(year: i32, month: i32, day: i32) -> i32 {
-        let current_year = 2021;
-        let current_month = 9;
-        let current_day = 6;
+        let current_year = 2024;
+        let current_month = 12;
+        let current_day = 19;
         let mut age = current_year - year;
         if month > current_month {
             age -= 1;
